0ad5c87d9b2bac5bed0714abeb453eea325a7bb2
DC-21 <cholahkuboko@gmail.com>
2023-10-15 10:58:43 +0200

    verson 2.1

--- Controllers/v2/WeatherForecastController.cs
+++ Controllers/v2/WeatherForecastController.cs
@@ -5,6 +5,7 @@ namespace WebVersioning.Api.Controllers.v2;
 [ApiController]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("2.0")]
+[ApiVersion("2.1")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -20,13 +21,26 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [MapToApiVersion("2.0")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = "the weather is cool as a summer breeze"
+                Summary = "the weather is cool as a summer breeze-v2"
+            })
+            .ToArray();
+    }
+    [HttpGet(Name = "GetWeatherForecastV21")]
+    [MapToApiVersion("2.1")]
+    public IEnumerable<WeatherForecast> GetV21()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = "the weather is hot as a summer breeze-v2"
             })
             .ToArray();
     }
